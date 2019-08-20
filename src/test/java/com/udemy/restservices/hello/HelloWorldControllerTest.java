ff6f2be43b5e28db631a8f96f6bc0b631869c4df
paavosoeiro <paavomsoeiro@gmail.com>
2019-08-19 23:58:43 -0300

    Hello World unit test

---
+++ src/test/java/com/udemy/restservices/hello/HelloWorldControllerTest.java
@@ -0,0 +1,39 @@
+package com.udemy.restservices.hello;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class HelloWorldControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+
+	@Test
+	public void testHelloWorld() throws Exception {
+		mockMvc.perform(get("/helloworld")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testHelloWorldBean() throws Exception {
+		mockMvc.perform(get("/helloworld-bean")).andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(jsonPath("$.firstName", is(equalTo("Paavo"))));
+	}
+
+}
